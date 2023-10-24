751104b2fd4ff6d18d014640f5b41b6511415e9a
PeroXide <103697143+Peroxidelol@users.noreply.github.com>
2023-10-24 03:01:00 -0700

    Update Compile.bat

--- Compile.bat
+++ Compile.bat
@@ -14,8 +14,15 @@ nimble install dimscord
 nimble install nimprotect
 nimble install winim
 nimble install pixie
+echo [92mModules succesfully installed[0m
 timeout 2 > nul
-
+echo.
+echo    _____ ___.               .___  .___             \u001b[35m _________  ________  \u001b[0m
+echo   /  _  \\_ |__ _____     __| _/__| _/____   ____  \u001b[35m \_   ___ \ \_____  \ \u001b[0m
+echo  /  /_\  \| __ \\__  \   / __ |/ __ |/  _ \ /    \ \u001b[35m /    \  \/  /  ____/ \u001b[0m
+echo /    |    \ \_\ \/ __ \_/ /_/ / /_/ (  <_> )   |  \\u001b[35m \     \____/       \ \u001b[0m
+echo \____|__  /___  (____  /\____ \____ |\____/|___|  /\u001b[35m  \______  /\_______ \\u001b[0m
+echo         \/    \/     \/      \/    \/           \/ \u001b[35m         \/         \/\u001b[0m
 echo.
 echo [92mCompiling source...[0m
 nim c .\Bot.nim
